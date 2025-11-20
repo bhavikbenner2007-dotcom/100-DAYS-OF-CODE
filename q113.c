bbee0ab8039a661aed291e366f5b447591ab56e5
BHAVIK <bhavikbenner2007@gmail.com>
2025-11-20 23:28:01 +0530

    Add files via upload

---
+++ q113.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter array elements: ");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter k: ");
+    scanf("%d", &k);
+
+    // Simple bubble sort
+    for(int i = 0; i < n-1; i++) {
+        for(int j = 0; j < n-i-1; j++) {
+            if(arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+
+    printf("The %dth smallest element is: %d\n", k, arr[k-1]);
+
+    return 0;
+}
