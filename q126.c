d0f7ad7820f2616e27cb263563be3515364a2322
BHAVIK <bhavikbenner2007@gmail.com>
2025-11-27 21:52:57 +0530

    Add files via upload

---
+++ q126.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    FILE *fp;
+    char filename[100], ch;
+    printf("Enter filename: ");
+    scanf("%s", filename);
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+        printf("Error: File not found\n");
+        return 1;
+    }
+    while ((ch = fgetc(fp)) != EOF) {
+        putchar(ch);
+    }
+    fclose(fp);
+    return 0;
+}
