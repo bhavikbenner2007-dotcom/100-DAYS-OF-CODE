2b43ad1d416a2736c62cdf38ae1049d24258ec77
BHAVIK <bhavikbenner2007@gmail.com>
2025-12-01 06:02:37 +0530

    Add files via upload

---
+++ q135.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+enum Numbers { TEN = 10, ELEVEN, TWELVE, THIRTEEN };
+
+int main() {
+    printf("%d\n", TEN);
+    printf("%d\n", ELEVEN);
+    printf("%d\n", TWELVE);
+    printf("%d\n", THIRTEEN);
+    return 0;
+}
